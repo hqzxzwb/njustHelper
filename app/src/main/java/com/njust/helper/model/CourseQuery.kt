7b5ea0ca14896f5097e9dc885f95c8945783be5e
zhuwenbo <zhuwenbo@qingting.fm>
2017-12-02 16:12:47 +0800

    migrate annotationProcess to kapt

---
+++ app/src/main/java/com/njust/helper/model/CourseQuery.kt
@@ -0,0 +1,10 @@
+package com.njust.helper.model
+
+class CourseQuery {
+    var name: String = ""
+    var teacher: String = ""
+    var classroom: String = ""
+    var week1: String = ""
+    var section: Int = 0
+    var day: Int = 0
+}
---
+++ app/src/main/java/com/njust/helper/model/Exam.kt
@@ -0,0 +1,8 @@
+package com.njust.helper.model
+
+class Exam {
+    var course: String = ""
+    var time: String = ""
+    var room: String = ""
+    var seat: String = ""
+}
