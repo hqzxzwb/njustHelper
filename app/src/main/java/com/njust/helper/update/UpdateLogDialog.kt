efc981ad5342d573f23d8dc378407df66ac7a825
zhuwenbo <zhuwenbo@qingting.fm>
2018-08-26 04:27:03 +0800

    Modify upgrade log

--- app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
+++ app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
@@ -9,7 +9,7 @@ import com.njust.helper.R
 object UpdateLogDialog {
     private const val message =
             "${BuildConfig.VERSION_CODE} v${BuildConfig.VERSION_NAME}更新日志：\n" +
-                    "1.修复 修复检查到更新后频繁弹通知提醒的问题\n"
+                    "由于一些疏忽，大部分功能暂时处于瘫痪状态。此版本对课表导入进行了紧急修复，其他功能将在后续版本尽快修复。十分抱歉。\n"
 
     @JvmStatic
     fun showUpdateDialog(context: Context) {
