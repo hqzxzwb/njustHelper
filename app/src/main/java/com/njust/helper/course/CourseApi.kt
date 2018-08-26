0a14e4157f6d03e24be82177cbf98b174d1c2281
zhuwenbo <zhuwenbo@qingting.fm>
2018-08-26 18:35:20 +0800

    Ajust structure of api classes

--- app/src/main/java/com/njust/helper/course/CourseApi.kt
+++ app/src/main/java/com/njust/helper/course/CourseApi.kt
@@ -8,172 +8,167 @@ import com.njust.helper.tools.LoginErrorException
 import com.njust.helper.tools.ServerErrorException
 import com.zwb.commonlibs.rx.ioSubscribeUiObserve
 import io.reactivex.Single
-import okhttp3.JavaNetCookieJar
-import okhttp3.OkHttpClient
 import retrofit2.HttpException
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.*
-import java.net.CookieManager
-import java.net.CookiePolicy
 import java.security.MessageDigest
 
-interface CourseApi {
-    @GET("njlgdx/xk/LoginToXk")
-    fun requestLogin(
-            @Query("USERNAME") stuid: String,
-            @Query("PASSWORD") pwd: String,
-            @Query("method") method: String = "verify"
-    ): Single<String>
+object CourseApi {
+    private interface CourseApiService {
+        @GET("xk/LoginToXk")
+        fun requestLogin(
+                @Query("USERNAME") stuid: String,
+                @Query("PASSWORD") pwd: String,
+                @Query("method") method: String = "verify"
+        ): Single<String>
 
-    @FormUrlEncoded
-    @POST("njlgdx/xskb/xskb_list.do")
-    fun courses(
-            @Query("Ves632DSdyV") query1: String = "NEW_XSD_PYGL",
-            @Field("cj0701id") body1: String = "",
-            @Field("zc") body2: String = "",
-            @Field("demo") body3: String = "",
-            @Field("xnxq01id") body4: String = "2018-2019-1"
-    ): Single<String>
+        @FormUrlEncoded
+        @POST("xskb/xskb_list.do")
+        fun courses(
+                @Query("Ves632DSdyV") query1: String = "NEW_XSD_PYGL",
+                @Field("cj0701id") body1: String = "",
+                @Field("zc") body2: String = "",
+                @Field("demo") body3: String = "",
+                @Field("xnxq01id") body4: String = "2018-2019-1"
+        ): Single<String>
+    }
 
-    companion object {
-        private val API = run {
-            Apis.newRetrofitBuilder()
-                    .baseUrl("http://202.119.81.113:9080/")
-                    .addConverterFactory(ScalarsConverterFactory.create())
-                    .build()
-                    .create(CourseApi::class.java)!!
-        }
+    private val service = run {
+        Apis.newRetrofitBuilder()
+                .baseUrl("http://202.119.81.113:9080/njlgdx/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build()
+                .create(CourseApiService::class.java)!!
+    }
 
-        fun get(stuid: String, pwd: String): Single<CourseData> {
-            return API
-                    .requestLogin(stuid, md5(pwd).toUpperCase())
-                    .mapLogin()
-                    .flatMap { API.courses() }
-                    .map { mapParse(it) }
-                    .ioSubscribeUiObserve()
-        }
+    fun get(stuid: String, pwd: String): Single<CourseData> {
+        return service
+                .requestLogin(stuid, md5(pwd).toUpperCase())
+                .mapLogin()
+                .flatMap { service.courses() }
+                .map { mapParse(it) }
+                .ioSubscribeUiObserve()
+    }
 
-        private fun Single<String>.mapLogin(): Single<Unit> {
-            return this
-                    .onErrorResumeNext {
-                        if (it is HttpException) {
-                            if (it.code() / 100 == 3) {
-                                return@onErrorResumeNext Single.just("success")
-                            }
-                        }
-                        Single.error(ServerErrorException())
-                    }
-                    .map<Unit> {
-                        when {
-                            it == "success" -> Unit
-                            it.contains("<html xmlns=\"http://www.w3.org/1999/xhtml\">") -> throw LoginErrorException()
-                            else -> throw ServerErrorException()
+    private fun Single<String>.mapLogin(): Single<Unit> {
+        return this
+                .onErrorResumeNext {
+                    if (it is HttpException) {
+                        if (it.code() / 100 == 3) {
+                            return@onErrorResumeNext Single.just("success")
                         }
                     }
-        }
-
-        private fun mapParse(string: String): CourseData {
-            val tableRegex = Regex("""<table id="kbtable"[\s\S]*?</table>""")
-            val trRegex = Regex("""<tr>[\s\S]*?</tr>""")
-            val tdRegex = Regex("""<td[\s\S]*?</td>""")
-            val divRegex = Regex("""<div id=".*-2".*</div>""")
-            val brRegex = Regex(""">(.*?)<br/>""")
-            val teacherRegex = Regex("""'老师'>(.*?)</font>""")
-            val weekRegex = Regex("""'周次\(节次\)'>(.*?)</font>""")
-            val classroomRegex = Regex("""'教室'>(.*?)</font>""")
-
-            val table = tableRegex.find(string)!!.groupValues[0]
-            val sectionRegex = """第[一二三四五]大节""".toRegex()
-            val result1 = trRegex.findAll(table)
-                    .mapNotNullTo(arrayListOf()) { match ->
-                        match.groupValues[0].takeIf { it.contains(sectionRegex) }
-                    }
-            val locList = arrayListOf<CourseLoc>()
-            val courseMap = hashMapOf<String, CourseInfo>()
-            for (timeOfDay in 0 until 5) {
-                val loc = CourseLoc()
-                loc.sec1 = timeOfDay
-                loc.sec2 = timeOfDay
-                val match1 = tdRegex.findAll(result1[timeOfDay]).toList()
-                for (dayOfWeek in 0 until 7) {
-                    loc.day = dayOfWeek
-                    val match2 = divRegex
-                            .find(match1[dayOfWeek].groupValues[0])!!
-                            .groupValues[0]
-                            .split("-----")
-                    for (item in match2) {
-                        val courseInfo: CourseInfo
-                        val find = brRegex.find(item)
-                        if (find == null) {
-                            continue
-                        } else {
-                            courseInfo = CourseInfo()
-                            courseInfo.name = find.groupValues[1]
-                        }
-                        courseInfo.teacher = run {
-                            val result = teacherRegex.find(item) ?: return@run ""
-                            result.groupValues[1]
-                        }
-                        courseInfo.id = md5(courseInfo.name + courseInfo.teacher)
-                        courseMap[courseInfo.id] = courseInfo
-                        loc.id = courseInfo.id
-                        run {
-                            val result = weekRegex.find(item) ?: return@run "1(周)"
-                            result.groupValues[1]
-                        }
-                                .also { loc.week1 = it }
-                                .let { loc.week2 = analyseWeek(it) }
-                        loc.classroom = run {
-                            val result = classroomRegex.find(item) ?: return@run ""
-                            result.groupValues[1]
-                        }
-                        locList.add(loc.clone())
+                    Single.error(ServerErrorException())
+                }
+                .map<Unit> {
+                    when {
+                        it == "success" -> Unit
+                        it.contains("<html xmlns=\"http://www.w3.org/1999/xhtml\">") ->
+                            throw LoginErrorException()
+                        else -> throw ServerErrorException()
                     }
                 }
-            }
-            val courseData = CourseData()
-            courseData.infos = courseMap.map { it.value }
-            courseData.locs = locList
-            courseData.startdate = "2018-08-27"
-            return courseData
-        }
+    }
 
-        private fun md5(s: String): String {
-            val instance = MessageDigest.getInstance("MD5")
-            val digest: ByteArray = instance.digest(s.toByteArray())
-            val sb = StringBuilder()
-            for (b in digest) {
-                val i: Int = b.toInt() and 0xff
-                var hexString = Integer.toHexString(i)
-                if (hexString.length < 2) {
-                    hexString = "0$hexString"
+    private fun mapParse(string: String): CourseData {
+        val tableRegex = Regex("""<table id="kbtable"[\s\S]*?</table>""")
+        val trRegex = Regex("""<tr>[\s\S]*?</tr>""")
+        val tdRegex = Regex("""<td[\s\S]*?</td>""")
+        val divRegex = Regex("""<div id=".*-2".*</div>""")
+        val brRegex = Regex(""">(.*?)<br/>""")
+        val teacherRegex = Regex("""'老师'>(.*?)</font>""")
+        val weekRegex = Regex("""'周次\(节次\)'>(.*?)</font>""")
+        val classroomRegex = Regex("""'教室'>(.*?)</font>""")
+
+        val table = tableRegex.find(string)!!.groupValues[0]
+        val sectionRegex = """第[一二三四五]大节""".toRegex()
+        val result1 = trRegex.findAll(table)
+                .mapNotNullTo(arrayListOf()) { match ->
+                    match.groupValues[0].takeIf { it.contains(sectionRegex) }
                 }
-                sb.append(hexString)
-            }
-            return sb.toString()
-        }
-
-        private fun analyseWeek(string: String): String {
-            val builder = StringBuilder(" ")
-            val strings = Regex("""(.*)\(周\)""")
-                    .find(string)!!
-                    .groupValues[1]
-                    .split(",")
-            for (tstring in strings) {
-                val j = tstring.indexOf('-')
-                if (j == -1) {
-                    builder.append(tstring).append(' ')
-                } else {
-                    var k = tstring.substring(0, j).toInt()
-                    while (k <= tstring.substring(j + 1).toInt()) {
-                        builder.append(k).append(' ')
-                        k++
+        val locList = arrayListOf<CourseLoc>()
+        val courseMap = hashMapOf<String, CourseInfo>()
+        for (timeOfDay in 0 until 5) {
+            val loc = CourseLoc()
+            loc.sec1 = timeOfDay
+            loc.sec2 = timeOfDay
+            val match1 = tdRegex.findAll(result1[timeOfDay]).toList()
+            for (dayOfWeek in 0 until 7) {
+                loc.day = dayOfWeek
+                val match2 = divRegex
+                        .find(match1[dayOfWeek].groupValues[0])!!
+                        .groupValues[0]
+                        .split("-----")
+                for (item in match2) {
+                    val courseInfo: CourseInfo
+                    val find = brRegex.find(item)
+                    if (find == null) {
+                        continue
+                    } else {
+                        courseInfo = CourseInfo()
+                        courseInfo.name = find.groupValues[1]
                     }
+                    courseInfo.teacher = run {
+                        val result = teacherRegex.find(item) ?: return@run ""
+                        result.groupValues[1]
+                    }
+                    courseInfo.id = md5(courseInfo.name + courseInfo.teacher)
+                    courseMap[courseInfo.id] = courseInfo
+                    loc.id = courseInfo.id
+                    run {
+                        val result = weekRegex.find(item) ?: return@run "1(周)"
+                        result.groupValues[1]
+                    }
+                            .also { loc.week1 = it }
+                            .let { loc.week2 = analyseWeek(it) }
+                    loc.classroom = run {
+                        val result = classroomRegex.find(item) ?: return@run ""
+                        result.groupValues[1]
+                    }
+                    locList.add(loc.clone())
                 }
             }
-            return builder.toString()
         }
+        val courseData = CourseData()
+        courseData.infos = courseMap.map { it.value }
+        courseData.locs = locList
+        courseData.startdate = "2018-08-27"
+        return courseData
+    }
+
+    private fun md5(s: String): String {
+        val instance = MessageDigest.getInstance("MD5")
+        val digest: ByteArray = instance.digest(s.toByteArray())
+        val sb = StringBuilder()
+        for (b in digest) {
+            val i: Int = b.toInt() and 0xff
+            var hexString = Integer.toHexString(i)
+            if (hexString.length < 2) {
+                hexString = "0$hexString"
+            }
+            sb.append(hexString)
+        }
+        return sb.toString()
+    }
+
+    private fun analyseWeek(string: String): String {
+        val builder = StringBuilder(" ")
+        val strings = Regex("""(.*)\(周\)""")
+                .find(string)!!
+                .groupValues[1]
+                .split(",")
+        for (tstring in strings) {
+            val j = tstring.indexOf('-')
+            if (j == -1) {
+                builder.append(tstring).append(' ')
+            } else {
+                var k = tstring.substring(0, j).toInt()
+                while (k <= tstring.substring(j + 1).toInt()) {
+                    builder.append(k).append(' ')
+                    k++
+                }
+            }
+        }
+        return builder.toString()
     }
 }
--- app/src/main/java/com/njust/helper/library/LibraryApi.kt
+++ app/src/main/java/com/njust/helper/library/LibraryApi.kt
@@ -12,119 +12,118 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface LibraryApi {
-    @GET("search_adv_result.php")
-    fun search(
-            @Query("q0") keyword: String,
-            @Query("sType0") q1: String = "any",
-            @Query("pageSize") q2: String = "100",
-            @Query("sort") q3: String = "score",
-            @Query("desc") q4: String = "true"
-    ): Single<String>
+object LibraryApi {
+    private interface LibraryApiService {
+        @GET("search_adv_result.php")
+        fun search(
+                @Query("q0") keyword: String,
+                @Query("sType0") q1: String = "any",
+                @Query("pageSize") q2: String = "100",
+                @Query("sort") q3: String = "score",
+                @Query("desc") q4: String = "true"
+        ): Single<String>
 
-    @GET("item.php")
-    fun detail(
-            @Query("marc_no") id: String
-    ): Single<String>
-
-    companion object {
-        private val API = Apis.newRetrofitBuilder()
-                .baseUrl("http://202.119.83.14:8080/uopac/opac/")
-                .addConverterFactory(ScalarsConverterFactory.create())
-                .build()
-                .create(LibraryApi::class.java)
-
-        fun search(keyword: String): Single<List<LibSearchBean>> {
-            return API.search(keyword)
-                    .map { parseSearch(it) }
-                    .ioSubscribeUiObserve()
-        }
-
-        fun detail(id: String): Single<LibDetailData> {
-            return API.detail(id)
-                    .map { parseDetail(it) }
-                    .ioSubscribeUiObserve()
-        }
+        @GET("item.php")
+        fun detail(
+                @Query("marc_no") id: String
+        ): Single<String>
     }
-}
 
-private fun parseSearch(string: String): List<LibSearchBean> {
-    if (!string.contains("南京理工大学图书馆")) {
-        throw ServerErrorException()
+    private val service = Apis.newRetrofitBuilder()
+            .baseUrl("http://202.119.83.14:8080/uopac/opac/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+            .create(LibraryApiService::class.java)
+
+    fun search(keyword: String): Single<List<LibSearchBean>> {
+        return service.search(keyword)
+                .map { parseSearch(it) }
+                .ioSubscribeUiObserve()
     }
-    return Regex("""href="item.php\?marc_no=(\d*)">([^<]*)<.*\s*.*>(.*)<.*\s*.*>(.*)<""")
-            .findAll(string)
-            .mapTo(arrayListOf()) {
-                val groupValues = it.groupValues
-                LibSearchBean(
-                        title = groupValues[2],
-                        author = groupValues[3],
-                        press = groupValues[4],
-                        id = groupValues[1]
+
+    fun detail(id: String): Single<LibDetailData> {
+        return service.detail(id)
+                .map { parseDetail(it) }
+                .ioSubscribeUiObserve()
+    }
+
+    private fun parseSearch(string: String): List<LibSearchBean> {
+        if (!string.contains("南京理工大学图书馆")) {
+            throw ServerErrorException()
+        }
+        return Regex("""href="item.php\?marc_no=(\d*)">([^<]*)<.*\s*.*>(.*)<.*\s*.*>(.*)<""")
+                .findAll(string)
+                .mapTo(arrayListOf()) {
+                    val groupValues = it.groupValues
+                    LibSearchBean(
+                            title = groupValues[2],
+                            author = groupValues[3],
+                            press = groupValues[4],
+                            id = groupValues[1]
+                    )
+                }
+    }
+
+    private fun parseDetail(string: String): LibDetailData {
+        val result = LibDetailData()
+
+        val headerBuilder = StringBuilder()
+        Regex("""<dt>题名/责任者:</dt>\s*<dd>(.*?)</dd>""")
+                .find(string)
+                ?.let {
+                    headerBuilder.append("题名/责任者:\n")
+                            .append(trimHtmlString(it.groupValues[1]))
+                            .append("\n\n")
+                }
+        Regex("""<dt>出版发行项:</dt>\s*<dd>(.*?)</dd>""")
+                .find(string)
+                ?.let {
+                    headerBuilder.append("出版发行项:\n")
+                            .append(trimHtmlString(it.groupValues[1]))
+                            .append("\n\n")
+                }
+        Regex("""<dt>ISBN及定价:</dt>\s*<dd>(.*?)</dd>""")
+                .find(string)
+                ?.let {
+                    headerBuilder.append("ISBN及定价:\n")
+                            .append(trimHtmlString(it.groupValues[1]))
+                            .append("\n\n")
+                }
+        Regex("""<dt>提要文摘附注:</dt>\s*<dd>(.*?)</dd>""")
+                .find(string)
+                ?.let {
+                    headerBuilder.append("提要文摘附注:\n")
+                            .append(trimHtmlString(it.groupValues[1]))
+                            .append("\n\n")
+                }
+        result.head = headerBuilder.toString()
+
+        val stateList = arrayListOf<LibDetailItem>()
+        val matches1 = Regex("""<tr align="left" class="whitetext"[\s\S]*?</tr>""")
+                .findAll(string)
+        val tdRegex = Regex("""<td.*?>[\s ]*([\s\S]*?)[\s ]*</td>""")
+        matches1.forEach {
+            val matches2 = tdRegex.findAll(it.groupValues[0]).toList()
+            stateList += if (matches2.size >= 5) {
+                LibDetailItem(
+                        code = trimHtmlString(matches2[0].groupValues[1]),
+                        place = trimHtmlString(matches2[3].groupValues[1]),
+                        state = matches2[4].groupValues[1].replace("应还日期", "应还")
+                )
+            } else {
+                LibDetailItem(
+                        code = matches2[0].groupValues[1],
+                        place = "",
+                        state = ""
                 )
             }
-}
-
-private fun parseDetail(string: String): LibDetailData {
-    val result = LibDetailData()
-
-    val headerBuilder = StringBuilder()
-    Regex("""<dt>题名/责任者:</dt>\s*<dd>(.*?)</dd>""")
-            .find(string)
-            ?.let {
-                headerBuilder.append("题名/责任者:\n")
-                        .append(trimHtmlString(it.groupValues[1]))
-                        .append("\n\n")
-            }
-    Regex("""<dt>出版发行项:</dt>\s*<dd>(.*?)</dd>""")
-            .find(string)
-            ?.let {
-                headerBuilder.append("出版发行项:\n")
-                        .append(trimHtmlString(it.groupValues[1]))
-                        .append("\n\n")
-            }
-    Regex("""<dt>ISBN及定价:</dt>\s*<dd>(.*?)</dd>""")
-            .find(string)
-            ?.let {
-                headerBuilder.append("ISBN及定价:\n")
-                        .append(trimHtmlString(it.groupValues[1]))
-                        .append("\n\n")
-            }
-    Regex("""<dt>提要文摘附注:</dt>\s*<dd>(.*?)</dd>""")
-            .find(string)
-            ?.let {
-                headerBuilder.append("提要文摘附注:\n")
-                        .append(trimHtmlString(it.groupValues[1]))
-                        .append("\n\n")
-            }
-    result.head = headerBuilder.toString()
-
-    val stateList = arrayListOf<LibDetailItem>()
-    val matches1 = Regex("""<tr align="left" class="whitetext"[\s\S]*?</tr>""")
-            .findAll(string)
-    val tdRegex = Regex("""<td.*?>[\s ]*([\s\S]*?)[\s ]*</td>""")
-    matches1.forEach {
-        val matches2 = tdRegex.findAll(it.groupValues[0])
-                .toList()
-        stateList += if (matches2.size >= 5) {
-            LibDetailItem(
-                    code = trimHtmlString(matches2[0].groupValues[1]),
-                    place = trimHtmlString(matches2[3].groupValues[1]),
-                    state = matches2[4].groupValues[1].replace("应还日期", "应还")
-            )
-        } else {
-            LibDetailItem(
-                    code = matches2[0].groupValues[1],
-                    place = "",
-                    state = ""
-            )
         }
+        result.states = stateList
+
+        return result
     }
-    result.states = stateList
 
-    return result
-}
-
-private fun trimHtmlString(input: String): String {
-    return Html.fromHtml(input).toString().trim()
+    private fun trimHtmlString(input: String): String {
+        return Html.fromHtml(input).toString().trim()
+    }
 }
