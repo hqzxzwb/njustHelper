4db3faed02414fdc9d49d3517e1e465e818c7b0a
zhuwenbo <zhuwenbo@qingting.fm>
2018-08-27 14:26:18 +0800

    Change update log

--- app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
+++ app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
@@ -9,7 +9,7 @@ import com.njust.helper.R
 object UpdateLogDialog {
     private const val message =
             "${BuildConfig.VERSION_CODE} v${BuildConfig.VERSION_NAME}更新日志：\n" +
-                    "由于一些疏忽，大部分功能暂时处于瘫痪状态。此版本对课表导入进行了紧急修复，其他功能将在后续版本尽快修复。十分抱歉。\n"
+                    "由于一些疏忽，大部分功能暂时处于瘫痪状态。此版本中课表导入、图书检索、课程和教室查询已恢复。其他功能将在后续版本尽快修复。十分抱歉。\n"
 
     @JvmStatic
     fun showUpdateDialog(context: Context) {
