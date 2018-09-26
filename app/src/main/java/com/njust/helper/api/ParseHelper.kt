62b331b7a54aeebec32b95c7270974038adc46cf
zhuwenbo <zhuwenbo@qingting.fm>
2018-09-26 11:55:00 +0800

    Report parse error to bugly

--- app/src/main/java/com/njust/helper/api/ParseHelper.kt
+++ app/src/main/java/com/njust/helper/api/ParseHelper.kt
@@ -1,9 +1,14 @@
 package com.njust.helper.api
 
+import com.tencent.bugly.crashreport.BuglyLog
+import com.tencent.bugly.crashreport.CrashReport
+
 inline fun <T> parseReportingError(s: String, parser: (String) -> T): T {
     try {
         return parser(s)
     } catch (t: Throwable) {
+        BuglyLog.e("ParseFailed", s)
+        CrashReport.postCatchedException(Exception(s, t))
         throw ParseErrorException()
     }
 }
