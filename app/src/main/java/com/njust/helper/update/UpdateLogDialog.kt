51e27d74866209196b064e3b82f2c05b648e955e
zhuwenbo <zhuwenbo@qingting.fm>
2018-09-27 14:26:05 +0800

    Release note for v5.9.27

--- app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
+++ app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
@@ -9,7 +9,9 @@ import com.njust.helper.R
 object UpdateLogDialog {
     private const val message =
             "${BuildConfig.VERSION_CODE} v${BuildConfig.VERSION_NAME}更新日志：\n" +
-                    "恢复先前失效的功能\n"
+                    "1.成绩查询显示总GPA\n" +
+                    "2.修复部分成绩值无法解析的问题\n" +
+                    "3.修复图书借阅查询"
 
     @JvmStatic
     fun showUpdateDialog(context: Context) {
