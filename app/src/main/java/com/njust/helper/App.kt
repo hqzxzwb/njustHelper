226b1dd6a4d3a8a7e5dcf2e1c440a1415f99b89d
zhuwenbo <zhuwenbo@qingting.fm>
2018-11-03 19:37:02 +0800

    Move vector drawables to `drawable` folder to resolve compatibility issues.

--- app/src/main/java/com/njust/helper/App.kt
+++ app/src/main/java/com/njust/helper/App.kt
@@ -2,11 +2,14 @@ package com.njust.helper
 
 import android.app.Application
 import android.content.Intent
+import androidx.appcompat.app.AppCompatDelegate
 import com.tencent.bugly.crashreport.CrashReport
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
 
         startService(Intent(this, BackgroundService::class.java)
                 .putExtra("action", "registerReceiver"))
