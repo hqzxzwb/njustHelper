e2dbf9b493085095857559aa6aafc07b14c9d0bd
zhuwenbo <zhuwenbo@qingting.fm>
2019-02-24 14:04:41 +0800

    Update log

--- app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
+++ app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
@@ -9,8 +9,9 @@ import com.njust.helper.R
 object UpdateLogDialog {
     private const val message =
             "${BuildConfig.VERSION_CODE} v${BuildConfig.VERSION_NAME}更新日志：\n" +
-                    "1.修复 图书检索\n" +
-                    "2.新增 桌面图标快捷方式\n"
+                    "1.修复 修复未评教时无法查询成绩的问题\n" +
+                    "2.修复 部分成绩值无法解析的问题\n" +
+                    "3.修复 导入新学期课表\n"
 
     @JvmStatic
     fun showUpdateDialog(context: Context) {
