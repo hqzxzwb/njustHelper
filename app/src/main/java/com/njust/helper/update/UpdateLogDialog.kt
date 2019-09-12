c0fd06beda908c8112571b3c6490c55603fa57a3
zhuwenbo <zhuwenbo@qingting.fm>
2019-09-13 01:20:14 +0800

    Upgrade log

--- app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
+++ app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
@@ -9,8 +9,7 @@ import com.njust.helper.R
 object UpdateLogDialog {
     private const val message =
             "${BuildConfig.VERSION_CODE} v${BuildConfig.VERSION_NAME}更新日志：\n" +
-                    "1.修复 课程时间显示\n" +
-                    "2.修复 教室与课程查询\n"
+                    "1.修复 图书查询不显示借阅状况的问题\n"
 
     @JvmStatic
     fun showUpdateDialog(context: Context) {
