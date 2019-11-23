6d8026d5eec2e02db6b1038a35f359db76769307
zhuwenbo <zhuwenbo@qingting.fm>
2019-11-23 23:44:23 +0800

    AGP 3.5.2; disable compat vector

--- app/src/main/java/com/njust/helper/App.kt
+++ app/src/main/java/com/njust/helper/App.kt
@@ -1,15 +1,12 @@
 package com.njust.helper
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.njust.helper.settings.CourseAlarms
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
 
         CourseAlarms.registerCourseAlarm(this)
 
