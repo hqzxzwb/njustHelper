aedc6c2ca771e091ee1974b099dc29e94d1eb4a6
hqzxzwb <hqzxzwb@163.com>
2020-07-25 09:42:47 +0800

    Add umeng

--- app/src/main/java/com/njust/helper/App.kt
+++ app/src/main/java/com/njust/helper/App.kt
@@ -3,6 +3,7 @@ package com.njust.helper
 import android.app.Application
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.njust.helper.settings.CourseAlarms
+import com.umeng.commonsdk.UMConfigure
 
 class App : Application() {
     override fun onCreate() {
@@ -10,6 +11,7 @@ class App : Application() {
 
         CourseAlarms.registerCourseAlarm(this)
 
+        UMConfigure.init(this, getString(R.string.umengAppKey), null, 0, null)
         FirebaseRemoteConfig.getInstance().setDefaults(R.xml.remote_config_defaults)
     }
 }
