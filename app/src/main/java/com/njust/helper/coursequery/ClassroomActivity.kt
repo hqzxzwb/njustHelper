b9b748e3688f30ff06853fbf0a76c14f7b8683a7
hqzxzwb <hqzxzwb@163.com>
2021-12-06 09:29:53 +0800

    Compose ClassroomActivity

--- app/src/main/java/com/njust/helper/coursequery/ClassroomActivity.kt
+++ app/src/main/java/com/njust/helper/coursequery/ClassroomActivity.kt
@@ -1,9 +1,17 @@
 package com.njust.helper.coursequery
 
+import android.content.Context
 import android.os.Bundle
-import android.view.View
-import android.widget.CheckBox
-import androidx.databinding.DataBindingUtil
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import com.njust.helper.R
 import com.njust.helper.RemoteConfig
@@ -19,16 +27,13 @@ import kotlinx.coroutines.launch
  * @author zwb
  */
 class ClassroomActivity : BaseActivity() {
-  private lateinit var checkBoxes: Array<CheckBox>
-
   lateinit var binding: ActivityClassroomBinding
+  private val viewModel by viewModels<ClassroomViewModel>()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     val time = (System.currentTimeMillis() - RemoteConfig.getTermStartTime()) % TimeUtil.ONE_DAY
-    val captions = resources.getStringArray(R.array.sections)
-    checkBoxes.forEachIndexed { index, checkBox -> checkBox.text = captions[index] }
 
     var i = 0
     while (i < Constants.COURSE_SECTION_COUNT) {
@@ -37,88 +42,178 @@ class ClassroomActivity : BaseActivity() {
       }
       i++
     }
+    var selectedSections = 0
     if (i < Constants.COURSE_SECTION_COUNT) {
-      checkBoxes[i].isChecked = true
+      selectedSections = selectedSections or (1 shl i)
       if (i < 4 && 2 * time > Constants.SECTION_START[i] + Constants.SECTION_END[i]) {
-        checkBoxes[i + 1].isChecked = true
+        selectedSections = selectedSections or (1 shl (i + 1))
       }
     }
+    viewModel.selectedSections.setValue(viewModel, viewModel::selectedSections, selectedSections)
   }
 
   override fun layoutRes(): Int = 0
 
   override fun layout() {
-    binding = DataBindingUtil.setContentView(this, R.layout.activity_classroom)
-
-    checkBoxes = arrayOf(
-        binding.checkBox1,
-        binding.checkBox2,
-        binding.checkBox3,
-        binding.checkBox4,
-        binding.checkBox5
-    )
-
-    binding.button1.setOnClickListener { onClickQueryButton() }
+    setContent { Screen(viewModel) }
   }
 
-  private fun onClickQueryButton() {
-    val sections = checkBoxes
-        .foldIndexed(0) { index, acc, checkBox ->
-          if (checkBox.isChecked) {
-            acc or (1 shl index)
-          } else {
-            acc
-          }
+  @Composable
+  private fun Screen(viewModel: ClassroomViewModel) {
+    val result = viewModel.resultText
+    MaterialTheme {
+      Column {
+        ControlCard(viewModel)
+        ResultCard(result)
+      }
+      QueryButton {
+        lifecycleScope.launch {
+          viewModel.query(this@ClassroomActivity)
         }
-    if (sections == 0) {
-      showSnack(R.string.toast_cr_choose_one_section)
-      return
+      }
     }
-    val dayId = when (binding.dateGroup.checkedRadioButtonId) {
-      R.id.radio0 -> 0
-      R.id.radio1 -> 1
-      else -> 2
+  }
+
+  @Composable
+  private fun ControlCard(viewModel: ClassroomViewModel) {
+    val selectedDay = viewModel.selectedDay
+    val selectedBuilding = viewModel.selectedBuilding
+    val selectedSections = viewModel.selectedSections
+    Card {
+      Column {
+        Text(text = stringResource(R.string.label_classroom_date))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+          DayRadioButton(
+            selectedDayState = selectedDay,
+            index = 0,
+            text = stringResource(R.string.radio_classroom_today)
+          )
+          DayRadioButton(
+            selectedDayState = selectedDay,
+            index = 1,
+            text = stringResource(R.string.radio_classroom_tomorrow)
+          )
+          DayRadioButton(
+            selectedDayState = selectedDay,
+            index = 2,
+            text = stringResource(R.string.radio_classroom_day_after)
+          )
+        }
+        Text(text = stringResource(R.string.label_classroom_buildings))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+          BuildingRadioButton(
+            selectedBuildingState = selectedBuilding,
+            index = 0,
+            text = stringResource(R.string.radio_classroom_building_four)
+          )
+          BuildingRadioButton(
+            selectedBuildingState = selectedBuilding,
+            index = 1,
+            text = stringResource(R.string.radio_classroom_building_two)
+          )
+          BuildingRadioButton(
+            selectedBuildingState = selectedBuilding,
+            index = 2,
+            text = stringResource(R.string.radio_classroom_building_one)
+          )
+        }
+        Text(text = stringResource(R.string.label_classroom_section))
+        stringArrayResource(id = R.array.sections)
+          .forEachIndexed { index, s ->
+            SectionCheckBox(selectedSectionsState = selectedSections, index = index, text = s)
+          }
+      }
     }
+  }
+
+  @Composable
+  private fun DayRadioButton(selectedDayState: MutableState<Int>, index: Int, text: String) {
+    var selectedDay by remember { selectedDayState }
+    RadioButton(selected = selectedDay == index, onClick = { selectedDay = index })
+    Text(text = text)
+  }
+
+  @Composable
+  private fun BuildingRadioButton(
+    selectedBuildingState: MutableState<Int>,
+    index: Int,
+    text: String
+  ) {
+    var selectedBuilding by remember {
+      selectedBuildingState
+    }
+    RadioButton(selected = selectedBuilding == index, onClick = { selectedBuilding = index })
+    Text(text = text)
+  }
+
+  @Composable
+  private fun SectionCheckBox(selectedSectionsState: MutableState<Int>, index: Int, text: String) {
+    var selectedSections by remember {
+      selectedSectionsState
+    }
+    val maskedIndex = 1 shl index
+    Row {
+      Checkbox(checked = (selectedSections and maskedIndex) != 0, onCheckedChange = {
+        selectedSections = if (it) {
+          selectedSections or maskedIndex
+        } else {
+          selectedSections and maskedIndex.inv()
+        }
+      })
+      Text(text = text)
+    }
+  }
+
+  @Composable
+  private fun ResultCard(resultText: String) {
+    Card {
+      Text(text = resultText)
+    }
+  }
+
+  @Composable
+  private fun QueryButton(onClick: () -> Unit) {
+    FloatingActionButton(onClick = onClick) {
+
+    }
+  }
+}
+
+class ClassroomViewModel : ViewModel() {
+  var selectedDay = mutableStateOf(0)
+  var selectedBuilding = mutableStateOf(0)
+  var selectedSections = mutableStateOf(0)
+  var resultText by mutableStateOf("")
+    private set
+
+  private val BUILDING_VALUE = arrayOf("Ⅳ", "II", "I")
+
+  suspend fun query(context: Context) {
+    val sections = selectedSections.value
+//    if (sections == 0) {
+//      showSnack(R.string.toast_cr_choose_one_section)
+//      return
+//    }
+    val dayId = selectedDay.value
     val dateLong = System.currentTimeMillis() + TimeUtil.ONE_DAY * dayId
     val termStart = RemoteConfig.getTermStartTime()
     val dayIndex = ((dateLong - termStart) / TimeUtil.ONE_DAY).toInt()
     val week = dayIndex / 7 + 1
     val day = dayIndex % 7
-    val buildingIndex = when (binding.buildingGroup.checkedRadioButtonId) {
-      R.id.radio3 -> 0
-      R.id.radio4 -> 1
-      else -> 2
-    }
-    val building = BUILDING_VALUE[buildingIndex]
-    binding.loading = true
-    lifecycleScope.launch {
-      try {
-        val dao = CourseQueryDao.getInstance(this@ClassroomActivity)
-        val allRooms = dao.queryClassroomSet(building)
-        val ruledOutRooms = dao.queryClassroom(building, week, day, sections)
-        val result = (allRooms - ruledOutRooms)
-            .fold(StringBuilder()) { acc, it ->
-              acc.append(it.replace('-', '_')).append("  ")
-            }
-            .toString()
-        binding.loading = false
-        if (result == "") {
-          binding.text = getString(R.string.text_classroom_no_info)
-        } else {
-          binding.text = result
-        }
-      } catch (e: Exception) {
-        binding.loading = false
-        binding.text = getString(R.string.text_classroom_fail)
+    try {
+      val dao = CourseQueryDao.getInstance(context)
+      val building = BUILDING_VALUE[selectedBuilding.value]
+      val allRooms = dao.queryClassroomSet(building)
+      val ruledOutRooms = dao.queryClassroom(building, week, day, sections).toSet()
+      val result = (allRooms - ruledOutRooms)
+        .joinToString(separator = "  ") { it.replace('-', '_') }
+      resultText = if (result.isBlank()) {
+        context.getString(R.string.text_classroom_no_info)
+      } else {
+        result
       }
+    } catch (e: Exception) {
+      resultText = context.getString(R.string.text_classroom_fail)
     }
-  }
-
-  override fun getViewForSnackBar(): View {
-    return binding.coordinatorLayout
-  }
-
-  companion object {
-    private val BUILDING_VALUE = arrayOf("Ⅳ", "II", "I")
   }
 }
