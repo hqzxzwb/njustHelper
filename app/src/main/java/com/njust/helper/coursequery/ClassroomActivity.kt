fb595a2a70c38c4ac8487f9db49ceda3a44dae6a
hqzxzwb <hqzxzwb@163.com>
2021-12-08 01:17:18 +0800

    调整compose布局

--- app/src/main/java/com/njust/helper/coursequery/ClassroomActivity.kt
+++ app/src/main/java/com/njust/helper/coursequery/ClassroomActivity.kt
@@ -4,21 +4,32 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import com.njust.helper.R
 import com.njust.helper.RemoteConfig
 import com.njust.helper.activity.BaseActivity
-import com.njust.helper.databinding.ActivityClassroomBinding
 import com.njust.helper.tools.Constants
 import com.njust.helper.tools.TimeUtil
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 /**
@@ -27,7 +38,6 @@ import kotlinx.coroutines.launch
  * @author zwb
  */
 class ClassroomActivity : BaseActivity() {
-  lateinit var binding: ActivityClassroomBinding
   private val viewModel by viewModels<ClassroomViewModel>()
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,6 +60,13 @@ class ClassroomActivity : BaseActivity() {
       }
     }
     viewModel.selectedSections.setValue(viewModel, viewModel::selectedSections, selectedSections)
+    lifecycleScope.launchWhenCreated {
+      viewModel.noSectionChosenPublisher.collect {
+        viewModel.snackbarHostState.showSnackbar(
+          getString(R.string.toast_cr_choose_one_section)
+        )
+      }
+    }
   }
 
   override fun layoutRes(): Int = 0
@@ -61,40 +78,70 @@ class ClassroomActivity : BaseActivity() {
   @Composable
   private fun Screen(viewModel: ClassroomViewModel) {
     val result = viewModel.resultText
+    val snackbarHostState = viewModel.snackbarHostState
     MaterialTheme {
-      Column {
-        ControlCard(viewModel)
-        ResultCard(result)
-      }
-      QueryButton {
-        lifecycleScope.launch {
-          viewModel.query(this@ClassroomActivity)
+      Scaffold(
+        floatingActionButton = {
+          QueryButton {
+            lifecycleScope.launch {
+              viewModel.query(this@ClassroomActivity)
+            }
+          }
+        },
+        snackbarHost = {
+          SnackbarHost(
+            hostState = snackbarHostState
+          )
+        },
+        content = {
+          Column(
+            modifier = Modifier
+              .scrollable(state = rememberScrollState(), orientation = Orientation.Vertical)
+          ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            ControlCard(
+              viewModel.selectedDay,
+              viewModel.selectedBuilding,
+              viewModel.selectedSections,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            ResultCard(
+              result
+            )
+            Spacer(modifier = Modifier.height(88.dp))
+          }
         }
-      }
+      )
     }
   }
 
   @Composable
-  private fun ControlCard(viewModel: ClassroomViewModel) {
-    val selectedDay = viewModel.selectedDay
-    val selectedBuilding = viewModel.selectedBuilding
-    val selectedSections = viewModel.selectedSections
-    Card {
+  private fun ControlCard(
+    selectedDayState: MutableState<Int>,
+    selectedBuildingState: MutableState<Int>,
+    selectedSectionsState: MutableState<Int>,
+  ) {
+    Card(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp),
+      elevation = 2.dp,
+    ) {
       Column {
         Text(text = stringResource(R.string.label_classroom_date))
         Row(verticalAlignment = Alignment.CenterVertically) {
           DayRadioButton(
-            selectedDayState = selectedDay,
+            selectedDayState = selectedDayState,
             index = 0,
             text = stringResource(R.string.radio_classroom_today)
           )
           DayRadioButton(
-            selectedDayState = selectedDay,
+            selectedDayState = selectedDayState,
             index = 1,
             text = stringResource(R.string.radio_classroom_tomorrow)
           )
           DayRadioButton(
-            selectedDayState = selectedDay,
+            selectedDayState = selectedDayState,
             index = 2,
             text = stringResource(R.string.radio_classroom_day_after)
           )
@@ -102,17 +149,17 @@ class ClassroomActivity : BaseActivity() {
         Text(text = stringResource(R.string.label_classroom_buildings))
         Row(verticalAlignment = Alignment.CenterVertically) {
           BuildingRadioButton(
-            selectedBuildingState = selectedBuilding,
+            selectedBuildingState = selectedBuildingState,
             index = 0,
             text = stringResource(R.string.radio_classroom_building_four)
           )
           BuildingRadioButton(
-            selectedBuildingState = selectedBuilding,
+            selectedBuildingState = selectedBuildingState,
             index = 1,
             text = stringResource(R.string.radio_classroom_building_two)
           )
           BuildingRadioButton(
-            selectedBuildingState = selectedBuilding,
+            selectedBuildingState = selectedBuildingState,
             index = 2,
             text = stringResource(R.string.radio_classroom_building_one)
           )
@@ -120,7 +167,11 @@ class ClassroomActivity : BaseActivity() {
         Text(text = stringResource(R.string.label_classroom_section))
         stringArrayResource(id = R.array.sections)
           .forEachIndexed { index, s ->
-            SectionCheckBox(selectedSectionsState = selectedSections, index = index, text = s)
+            SectionCheckBox(
+              selectedSectionsState = selectedSectionsState,
+              index = index,
+              text = s,
+            )
           }
       }
     }
@@ -165,9 +216,17 @@ class ClassroomActivity : BaseActivity() {
   }
 
   @Composable
-  private fun ResultCard(resultText: String) {
-    Card {
-      Text(text = resultText)
+  private fun ResultCard(
+    resultText: String,
+  ) {
+    Card(
+      modifier = Modifier.fillMaxWidth(),
+      elevation = 2.dp,
+    ) {
+      Text(
+        modifier = Modifier.padding(8.dp),
+        text = resultText,
+      )
     }
   }
 
@@ -185,35 +244,39 @@ class ClassroomViewModel : ViewModel() {
   var selectedSections = mutableStateOf(0)
   var resultText by mutableStateOf("")
     private set
+  private val noSectionChosenFlow = MutableSharedFlow<Unit>()
+  val noSectionChosenPublisher: Flow<Unit>
+    get() = noSectionChosenFlow
+  val snackbarHostState by mutableStateOf(SnackbarHostState())
 
   private val BUILDING_VALUE = arrayOf("Ⅳ", "II", "I")
 
   suspend fun query(context: Context) {
     val sections = selectedSections.value
-//    if (sections == 0) {
-//      showSnack(R.string.toast_cr_choose_one_section)
-//      return
-//    }
+    if (sections == 0) {
+      noSectionChosenFlow.emit(Unit)
+      return
+    }
     val dayId = selectedDay.value
     val dateLong = System.currentTimeMillis() + TimeUtil.ONE_DAY * dayId
     val termStart = RemoteConfig.getTermStartTime()
     val dayIndex = ((dateLong - termStart) / TimeUtil.ONE_DAY).toInt()
     val week = dayIndex / 7 + 1
     val day = dayIndex % 7
-    try {
+    resultText = try {
       val dao = CourseQueryDao.getInstance(context)
       val building = BUILDING_VALUE[selectedBuilding.value]
       val allRooms = dao.queryClassroomSet(building)
       val ruledOutRooms = dao.queryClassroom(building, week, day, sections).toSet()
       val result = (allRooms - ruledOutRooms)
         .joinToString(separator = "  ") { it.replace('-', '_') }
-      resultText = if (result.isBlank()) {
+      if (result.isBlank()) {
         context.getString(R.string.text_classroom_no_info)
       } else {
         result
       }
     } catch (e: Exception) {
-      resultText = context.getString(R.string.text_classroom_fail)
+      context.getString(R.string.text_classroom_fail)
     }
   }
 }
