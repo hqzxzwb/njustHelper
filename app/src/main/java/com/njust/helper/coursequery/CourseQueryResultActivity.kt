c7900f2ff1fb80c5973c25acea8f325175f1fd9f
hqzxzwb <hqzxzwb@163.com>
2022-01-02 13:30:47 +0800

    Compose course query result activity

--- app/src/main/java/com/njust/helper/coursequery/CourseQueryResultActivity.kt
+++ app/src/main/java/com/njust/helper/coursequery/CourseQueryResultActivity.kt
@@ -1,18 +1,23 @@
 package com.njust.helper.coursequery
 
+import android.content.Context
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import com.njust.helper.R
-import com.njust.helper.activity.BaseActivity
-import com.njust.helper.databinding.ActivityCourseQueryResultBinding
-import com.njust.helper.tools.SimpleListVm
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 
-class CourseQueryResultActivity : BaseActivity() {
+class CourseQueryResultActivity : AppCompatActivity() {
   private var section: Int = 0
   private var day: Int = 0
-  private val vm = SimpleListVm<CourseQueryItemVm>()
+  private val viewModel by viewModels<CourseQueryResultViewModel>()
   private lateinit var name: String
   private lateinit var teacher: String
 
@@ -25,47 +30,50 @@ class CourseQueryResultActivity : BaseActivity() {
     day = if (dayOfWeek < 0) -1 else 1 shl dayOfWeek
     name = intent.getStringExtra("name")!!
     teacher = intent.getStringExtra("teacher")!!
+
+    query()
+
+    setContent {
+      CourseQueryResultScreen(
+        isRefreshing = viewModel.isRefreshing,
+        items = viewModel.items,
+        snackbarMessageFlow = viewModel.snackbarMessageFlow,
+        onRefresh = {
+          if (!viewModel.isRefreshing) {
+            query()
+          }
+        },
+        onClickHome = { finish() },
+      )
+    }
   }
 
-  override fun onPostCreate(savedInstanceState: Bundle?) {
-    super.onPostCreate(savedInstanceState)
-
-    refresh()
-  }
-
-  private fun refresh() {
+  private fun query() {
     lifecycleScope.launch {
-      try {
-        val data = CourseQueryDao.getInstance(this@CourseQueryResultActivity)
-            .queryCourses(name, teacher, section, day)
-        onDataReceived(data)
-      } catch (e: Exception) {
-        onError()
-      }
+      viewModel.query(this@CourseQueryResultActivity, name, teacher, section, day)
     }
-  }
-
-  private fun onDataReceived(list: List<CourseQueryItem>) {
-    vm.loading = false
-    if (list.isEmpty()) {
-      showSnack(R.string.message_no_result)
-    } else {
-      vm.items = list.mapIndexed { index, item -> CourseQueryItemVm(item, index) }
-    }
-  }
-
-  private fun onError() {
-    vm.loading = false
-    showSnack(R.string.message_net_error)
-  }
-
-  override fun layout() {
-    val binding = DataBindingUtil.setContentView<ActivityCourseQueryResultBinding>(this, R.layout.activity_course_query_result)
-    binding.recyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
-    binding.recyclerView.addItemDecoration(androidx.recyclerview.widget.DividerItemDecoration(this, androidx.recyclerview.widget.DividerItemDecoration.VERTICAL))
-    binding.swipeRefreshLayout.setOnRefreshListener(this::refresh)
-    binding.vm = vm
   }
 }
 
-class CourseQueryItemVm(val item: CourseQueryItem, val position: Int)
+class CourseQueryResultViewModel : ViewModel() {
+  var isRefreshing by mutableStateOf(false)
+  var items: List<CourseQueryItem> by mutableStateOf(listOf())
+  var snackbarMessageFlow = MutableSharedFlow<String>()
+
+  suspend fun query(context: Context, name: String, teacher: String, section: Int, day: Int) {
+    isRefreshing = true
+    try {
+      val data = CourseQueryDao.getInstance(context)
+        .queryCourses(name, teacher, section, day)
+      if (data.isEmpty()) {
+        snackbarMessageFlow.emit(context.getString(R.string.message_no_result))
+      } else {
+        items = data
+      }
+    } catch (e: Exception) {
+      isRefreshing = false
+      snackbarMessageFlow.emit(context.getString(R.string.message_net_error))
+    }
+    isRefreshing = false
+  }
+}
---
+++ app/src/main/java/com/njust/helper/coursequery/CourseQueryResultScreen.kt
@@ -0,0 +1,125 @@
+package com.njust.helper.coursequery
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.njust.helper.R
+import com.njust.helper.compose.DarkActionBarAppCompatTheme
+import com.zwb.commonlibs.utils.NoOpFunction
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
+
+@Composable
+fun CourseQueryResultScreen(
+  isRefreshing: Boolean,
+  items: List<CourseQueryItem>,
+  snackbarMessageFlow: Flow<String>,
+  onRefresh: () -> Unit,
+  onClickHome: () -> Unit,
+) {
+  val snackbarHostState = remember { SnackbarHostState() }
+  LaunchedEffect(key1 = snackbarHostState) {
+    snackbarMessageFlow.collectLatest {
+      snackbarHostState.showSnackbar(it)
+    }
+  }
+  DarkActionBarAppCompatTheme {
+    Scaffold(
+      topBar = {
+        TopAppBar(
+          navigationIcon = {
+            IconButton(onClick = onClickHome) {
+              Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+            }
+          },
+          title = { Text(stringResource(id = R.string.title_activity_courseQuery_result)) },
+        )
+      },
+      scaffoldState = rememberScaffoldState(snackbarHostState = snackbarHostState)
+    ) {
+      SwipeRefresh(
+        state = rememberSwipeRefreshState(isRefreshing = isRefreshing),
+        onRefresh = onRefresh,
+      ) {
+        LazyColumn {
+          itemsIndexed(items) { index, item ->
+            Column(
+              modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth(),
+            ) {
+              Text(text = "${index + 1}.${item.name}", style = MaterialTheme.typography.subtitle1)
+              Text(
+                text = stringResource(
+                  R.string.text_courseQuery_day_and_section,
+                  item.day + 1,
+                  item.section + 1,
+                ),
+                style = MaterialTheme.typography.body2,
+              )
+              Text(
+                text = item.teacher,
+                style = MaterialTheme.typography.body2,
+              )
+              Text(
+                text = item.classroom,
+                style = MaterialTheme.typography.body2,
+              )
+              Text(
+                text = item.week1,
+                style = MaterialTheme.typography.body2,
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+  CourseQueryResultScreen(
+    isRefreshing = false,
+    items = listOf(
+      CourseQueryItem(
+        0,
+        "II-101",
+        1,
+        2,
+        1,
+        2,
+        "name",
+        "teacher",
+        "week1",
+        "week2",
+      )
+    ),
+    snackbarMessageFlow = flow { },
+    onRefresh = NoOpFunction,
+    onClickHome = NoOpFunction,
+  )
+}
