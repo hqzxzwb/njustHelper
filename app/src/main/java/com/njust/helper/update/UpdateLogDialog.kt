481c1656146e66cee7d6cdd64a347bf25b528a16
zhuwenbo <zhuwenbo@bilibili.com>
2022-02-10 14:50:30 +0800

    Update upgrade log

--- app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
+++ app/src/main/java/com/njust/helper/update/UpdateLogDialog.kt
@@ -9,7 +9,9 @@ import com.njust.helper.R
 object UpdateLogDialog {
   private const val message =
       "${BuildConfig.VERSION_CODE} v${BuildConfig.VERSION_NAME}更新日志：\n" +
-          "1.修复 更新21-22学年第一学期数据\n"
+          "1.修复 更新21-22学年第二学期数据\n" +
+          "2.修复 图书可借状态查看\n" +
+          "3.新增 默认密码自动调整提示\n"
 
   @JvmStatic
   fun showUpdateDialog(context: Context) {
