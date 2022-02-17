4be6229e4173c42bc141f79f7e57ca7456105686
zhuwenbo <zhuwenbo@bilibili.com>
2022-02-17 19:41:56 +0800

    kotlin serialization plugin

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+    id("kotlinx-serialization")
 }
 
 kotlin {
