5ea5f6c635e8a400f3c2e85cd17745fc85765c64
zhuwenbo <zhuwenbo@bilibili.com>
2022-02-28 16:50:23 +0800

    Use multi threaded dispatcher for io

--- shared/src/iosMain/kotlin/com/njust/helper/shared/async/Dispatchers.kt
+++ shared/src/iosMain/kotlin/com/njust/helper/shared/async/Dispatchers.kt
@@ -1,6 +1,7 @@
 package com.njust.helper.shared.async
 
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.newFixedThreadPoolContext
 
-actual val ioDispatcher: CoroutineDispatcher = Dispatchers.Default
+actual val ioDispatcher: CoroutineDispatcher =
+  newFixedThreadPoolContext(10, "IODispatcherPool")
