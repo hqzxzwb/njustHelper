7ea17aa728b379bada51fce817096f50af77542e
zhuwenbo <hqzxzwb@gmail.com>
2022-05-08 00:04:17 +0800

    Remove obsolete tools

--- lib/src/main/java/com/zwb/commonlibs/binding/ViewHolders.kt
+++
@@ -1,8 +0,0 @@
-package com.zwb.commonlibs.binding
-
-import androidx.databinding.ViewDataBinding
-import androidx.recyclerview.widget.RecyclerView
-
-open class DataBindingHolder<T : ViewDataBinding>(val binding: T) : RecyclerView.ViewHolder(binding.root)
-
-class BaseDataBindingHolder(binding: ViewDataBinding) : DataBindingHolder<ViewDataBinding>(binding)
