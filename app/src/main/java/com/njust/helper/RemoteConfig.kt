86bd4cb8f05fbdc8e515c3449b89540032736110
zhuwenbo <zhuwenbo@bilibili.com>
2022-08-21 17:23:36 +0800

    2022-2023-1

--- app/src/main/java/com/njust/helper/RemoteConfig.kt
+++ app/src/main/java/com/njust/helper/RemoteConfig.kt
@@ -6,10 +6,10 @@ import java.util.*
 object RemoteConfig {
   private const val TAG = "RemoteConfig"
 
-  fun getTermId(): String = "2021-2022-2"
+  fun getTermId(): String = "2022-2023-1"
 
   fun getTermStartTime(): Long {
-    val dateString = "2022-02-21"
+    val dateString = "2022-08-22"
     val dd = SimpleDateFormat("yyyy-MM-dd", Locale.ROOT)
     val date = dd.parse(dateString)!!
     return date.time
