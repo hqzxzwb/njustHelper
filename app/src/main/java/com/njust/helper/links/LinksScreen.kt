0dae90eb23011540bfc5bea266962faef6f8d179
zhuwenbo <zhuwenbo@bilibili.com>
2023-01-19 21:52:46 +0800

    Upgrade compose

--- app/src/main/java/com/njust/helper/links/LinksScreen.kt
+++ app/src/main/java/com/njust/helper/links/LinksScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -15,6 +16,8 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -23,16 +26,15 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.njust.helper.R
-import com.njust.helper.compose.material.DarkActionBarAppCompatTheme
 import com.njust.helper.compose.emitOnAction
+import com.njust.helper.compose.material.DarkActionBarAppCompatTheme
 import com.njust.helper.shared.api.CommonLink
 import com.njust.helper.shared.links.LinksViewModel
 import com.zwb.commonlibs.utils.NoOpFunction
 import kotlinx.coroutines.flow.collectLatest
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun LinksScreen(
   vm: LinksViewModel,
@@ -48,7 +50,14 @@ fun LinksScreen(
   })
   DarkActionBarAppCompatTheme {
     Scaffold(
-      modifier = Modifier.fillMaxSize(),
+      modifier = Modifier
+        .fillMaxSize()
+        .pullRefresh(
+          rememberPullRefreshState(
+            refreshing = vm.loading,
+            onRefresh = vm.onRefreshAction.emitOnAction()
+          )
+        ),
       topBar = {
         TopAppBar(
           title = { Text(text = stringResource(R.string.title_activity_links)) },
@@ -63,17 +72,12 @@ fun LinksScreen(
         SnackbarHost(hostState = snackbarHostState)
       },
     ) {
-      SwipeRefresh(
-        modifier = Modifier.fillMaxSize(),
-        state = rememberSwipeRefreshState(isRefreshing = vm.loading),
-        onRefresh = vm.onRefreshAction.emitOnAction(),
+      LazyColumn(
+        modifier = Modifier.fillMaxSize()
+          .padding(it),
       ) {
-        LazyColumn(
-          modifier = Modifier.fillMaxSize(),
-        ) {
-          items(vm.items, null) { link ->
-            LinkItem(link = link, onClickLink = onClickLink)
-          }
+        items(vm.items, null) { link ->
+          LinkItem(link = link, onClickLink = onClickLink)
         }
       }
     }
